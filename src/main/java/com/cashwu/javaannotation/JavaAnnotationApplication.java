eed3ef09c72ab47984294b301b2bfadab21bc36d
Cash <cashwugeek@icloud.com>
2024-06-04 13:52:21 +0800

    init

---
+++ src/main/java/com/cashwu/javaannotation/JavaAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAnnotationApplication.class, args);
+    }
+
+}
